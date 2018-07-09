c0b3e2a96528ea4e0ad47a4185b85c44b43cf7d1
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-07-09 10:17:53 -0600

    created input for the user answer

--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import Input from './input';
+import {checkAnswer} from '../actions/protected-data';
 import userAnswer from '../actions/protected-data';
 // import {Field, reduxForm, focus} from 'redux-form';
 
 export class QuestionCard extends React.Component{
-  checkAnswer(){
-
+  constructor(props){
+    super(props);
+    this.input = React.createRef();
+  }
+  checkAnswer(e){
+    e.preventDefault();
+    let answer = this.input.current.value;
+    this.props.dispatch(checkAnswer(answer));
+    console.log(answer);
   }
   render(){
     return <section>
         <section>
           word in Espanol  
         </section>
-        
+        <form onSubmit={e => this.checkAnswer(e)}>
+        <label>
+          Name:
+          <input type="text" name="name"  ref={this.input}/>
+        </label>
+        {/* <input type="submit" value="Submit" /> */}
+      </form>
       </section>
   }
 }
