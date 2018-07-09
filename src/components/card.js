aaced750dcdbb96000020b83de3d075a8bdd09b5
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-07-09 10:02:34 -0600

    added action for checking answer of user

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Input from './input';
-import userAnswer from '../actions/'
+import userAnswer from '../actions/users';
+// import {Field, reduxForm, focus} from 'redux-form';
 
 export class QuestionCard extends React.Component{
+  checkAnswer(){
+
+  }
   render(){
-    return 
-      <section>
+    return <section>
         <section>
           word in Espanol  
         </section>
-
+            
       </section>
   }
 }
