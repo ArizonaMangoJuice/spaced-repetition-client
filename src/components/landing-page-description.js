ab60a062197e680be6e44703323a8189c164fc50
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-07-13 10:22:25 -0600

    added padding and styling to landingpage

--- src/components/landing-page-description.js
+++ src/components/landing-page-description.js
@@ -4,13 +4,13 @@ import './landing-page-description.css';
 
 export default function landingPageDescription() {
   return (
-    <div className="landing-page-description"> 
-      <div className="opacity-background">
-        <p> Create an account and start learning!</p>
-        <p> It's that simple! Input an answer, hit submit! </p>
-        <p> If your guess is correct, you'll see a message saying so and your score tally increment!</p> 
+    <section className="landing-page-description"> 
+      <section className="opacity-background">
+        <p>Create an account and start learning!</p>
+        <p>It's that simple! Input an answer, hit submit! </p>
+        <p>If your guess is correct, you'll see a message saying so and your score tally increment!</p> 
         <p>If your guess is incorrect, you'll see a message saying so, and your score will remain unchanged!</p>
-      </div>
-    </div>
+      </section>
+    </section>
   );
 }
