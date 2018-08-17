495bb1f75d95403e38b1c79f54828408c3604991
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-08-16 22:08:41 -0600

    added account for demo user

--- src/components/landing-page-description.js
+++ src/components/landing-page-description.js
@@ -7,6 +7,9 @@ export default function landingPageDescription() {
     <section className="landing-page-description"> 
       <section className="opacity-background">
         <p>Create an account and start learning!</p>
+        <p>Or you can use this demo account!</p>
+        <p>Username: Demo</p>
+        <p>password: password123</p>        
         <p>It's that simple! Input an answer, hit submit! </p>
         <p>If your guess is correct, you'll see a message saying so and your score tally increment!</p> 
         <p>If your guess is incorrect, you'll see a message saying so, and your score will remain unchanged!</p>
